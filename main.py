a1b6b2d5b7c4571c8a7f257b01a1a4ee2d5fdd35
Hoàng Nguyên <71932200+SleeplessCat182@users.noreply.github.com>
2022-01-25 23:01:43 +0700

    Update default path for windows music directory

--- main.py
+++ main.py
@@ -1,46 +1,62 @@
 from pytube import YouTube
 from pytube import Playlist
 from pathlib import Path
+import platform
 import os
 import subprocess
 import shutil
 
+defaultLinuxPath = str(Path.home()) + '/Music' 
+defaultWindowsPath = str(Path.home()) + '\\Music'
 
 urlList = open('urllist.txt','r')
 
-length = 0
 
-choice = input("Do you want to download a playlist ?(y/n)")
+def clean(title):
+    os.rename('test.mp3',title)
+    os.remove('Download_Video')
+    defaultPath = ''
+    choice = input("Do you want to choose the destination directory for downloaded musics ?(y/n)")
+    if choice == 'yes':
+        defaultPath = input()
+    elif platform.system() == "Linux":
+        defaultPath = defaultLinuxPath
+    else:
+        defaultPath = defaultWindowsPath
+    shutil.move(title,defaultPath)
 
-if choice == "n":
-    for url in urlList.readlines():
-        videoObject = YouTube(url)
-        print("Downloading: ",videoObject.title)
-        filteredMP4 = videoObject.streams.filter(progressive = True, file_extension ="mp4")
-        filteredMP4[0].download(filename="Download_Video")
-        ffmpeg = (f'ffmpeg -i Download_Video test.mp3')
-        subprocess.call(ffmpeg,shell=True)
-        title = videoObject.title.replace("/",'')+".mp3"
-        
-        #Cleaning and delete the downloaded video
-        os.rename('test.mp3',title)
-        os.remove('Download_Video')
-        shutil.move(title,'/home/nguyen/Music/')
-        length+=1
-else:
-    playlist_link = input("Playlist link: ");
-    playlist = Playlist(playlist_link)
-    for video in playlist.videos:
-        print("Downloading: ", video.title)
-        if(len(video.title)<=90):
-            title = video.title.replace("/",'')+".mp3"
-            video.streams.filter(progressive=True,file_extension="mp4")[0].download(filename="Download_Video")
+
+def process(choice):
+    length = 0
+    if choice == "n":
+        for url in urlList.readlines():
+            videoObject = YouTube(url)
+            print("Downloading: ",videoObject.title)
+            filteredMP4 = videoObject.streams.filter(progressive = True, file_extension ="mp4")
+            filteredMP4[0].download(filename="Download_Video")
             ffmpeg = (f'ffmpeg -i Download_Video test.mp3')
             subprocess.call(ffmpeg,shell=True)
-            #Cleaning and delete the downloaded video
-            os.rename('test.mp3',title)
-            os.remove('Download_Video')
-            shutil.move(title,'/home/nguyen/Music/')
+            title = videoObject.title.replace("/",'')+".mp3"
+            clean(title)
             length+=1
+    else:
+        playlist_link = input("Playlist link: ");
+        playlist = Playlist(playlist_link)
+        for video in playlist.videos:
+            print("Downloading: ", video.title)
+            if(len(video.title)<=90):
+                video.streams.filter(progressive=True,file_extension="mp4")[0].download(filename="Download_Video")
+                ffmpeg = (f'ffmpeg -i Download_Video test.mp3')
+                subprocess.call(ffmpeg,shell=True)
+                title = video.title.replace("/",'')+".mp3"
+                #Cleaning and delete the downloaded video
+                clean(title)
+                length+=1
+    print(f'Complete download {length} songs')
 
-print(f'Complete download {length} songs')
+def main():
+    choice = input("Do you want to download a playlist ?(y/n)")
+    process(choice)
+
+if __name__ == "__main__":
+    main()
